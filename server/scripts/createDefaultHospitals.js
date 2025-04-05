38c6db61a49d18db16bfd4edf1fc30fed49929a8
soncodez <chatgpt03012004@gmail.com>
2025-04-05 15:28:33 +0700

    1

--- server/scripts/createDefaultHospitals.js
+++ server/scripts/createDefaultHospitals.js
@@ -4,7 +4,7 @@
  */
 
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+require('dotenv').config();
 const mongoose = require('mongoose');
 const Hospital = require('../models/Hospital');
 
