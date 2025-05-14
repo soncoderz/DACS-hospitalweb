d2fe2ccf6ee58cb54ff49b4b67eb23db68e187db
Trịnh Công Sơn <145648237+soncoderz@users.noreply.github.com>
2025-05-14 14:24:20 +0700

    Delete server directory

--- server/app.js
+++
@@ -1,5 +0,0 @@
-// This file is deprecated. All code has been moved to server.js.
-// Please run server.js instead.
-
-console.log('app.js is deprecated. Please run server.js instead.');
-process.exit(0);
