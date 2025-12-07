91e4963ecf6fe2ed16823fd2c5c68843009491d8
Trịnh Công Sơn <145648237+soncoderz@users.noreply.github.com>
2025-12-07 11:35:31 +0700

    Delete server directory

--- server/routes/specialty.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const specialtyController = require('../controllers/specialtyController');
-// GET /api/specialties - Get all specialties
-router.get('/', specialtyController.getSpecialties);
-router.get('/:id', specialtyController.getSpecialtyById);
-module.exports = router; 
